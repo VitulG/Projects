c72719d2711557630fc1af2255c6b6680acd28ba
Vitul Gupta <guptavitul@gmail.com>
2024-11-28 00:57:16 +0530

    created the project House Hopper an app dedicated only for the real estate things

---
+++ src/test/java/com/rggupta/HouseHopper/HouseHopperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rggupta.HouseHopper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseHopperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
