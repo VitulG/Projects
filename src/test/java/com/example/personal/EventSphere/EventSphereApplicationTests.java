40991749a77ad8285f6a2f10f2ddc8cb30f13f0a
Vitul Gupta <guptavitul@gmail.com>
2024-11-27 21:42:53 +0530

    created the project EventSphere

---
+++ src/test/java/com/example/personal/EventSphere/EventSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.personal.EventSphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventSphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
