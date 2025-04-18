1c3b2d018645d9ad4269ba0476c1e3192e15c160
Vitul Gupta <guptavitul@gmail.com>
2025-04-18 17:16:44 +0530

    Real estate app

---
+++ src/main/java/com/rghousing/realestate/RgHousingApplication.java
@@ -0,0 +1,13 @@
+package com.rghousing.realestate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RgHousingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RgHousingApplication.class, args);
+	}
+
+}
